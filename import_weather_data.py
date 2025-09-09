26dfd069ffd3dbe09139c106c948efb58494694c
Christian Raßmann <rc@rassware.de>
2025-09-09 22:44:53 +0200

    revert to file processing

--- import_weather_data.py
+++ import_weather_data.py
@@ -5,17 +5,13 @@ import datetime
 import json
 import os
 import sqlite3
-import paho.mqtt.client as mqtt
 
-# Konfigurationsdatei einlesen
+directory = '/home/crassmann/transfer/'
+
 config = configparser.ConfigParser()
 home_dir = os.path.expanduser('~')
 credential_dir = os.path.join(home_dir, '.credentials')
 config.read(os.path.join(credential_dir, 'weather_upload'))
-
-MQTT_BROKER = config.get('MQTT', 'broker', fallback='localhost')
-MQTT_PORT = config.getint('MQTT', 'port', fallback=1883)
-MQTT_TOPIC = config.get('MQTT', 'topic', fallback='sensors/weather')
 
 
 class SensorData:
@@ -23,69 +19,44 @@ class SensorData:
         self.time = datetime.datetime.strptime(data['time'], '%Y-%m-%d %H:%M:%S')
         self.model = data['model']
         self.sensor_id = data['id']
-        self.channel = data.get('channel', "")
+        self.channel = ""
         self.temperature_C = ""
         self.temperature_C_dec = None
         self.humidity = ""
         self.humidity_dec = None
-
-        if 'temperature_C' in data:
+        if 'channel' in data.keys():
+            self.channel = data['channel']
+        if 'temperature_C' in data.keys():
             self.temperature_C = str(data['temperature_C'])
             self.temperature_C_dec = data['temperature_C']
-        if 'humidity' in data:
+        if 'humidity' in data.keys():
             self.humidity = str(data['humidity'])
             self.humidity_dec = data['humidity']
-        if 'temperature_F' in data:
+        if 'temperature_F' in data.keys():
             self.temperature_C = (data['temperature_F'] - 32) * 5 / 9
-            self.temperature_C_dec = float(self.temperature_C)
+            self.temperature_C_dec = float((data['temperature_F'] - 32) * 5 / 9)
 
     def __str__(self):
-        return ("time: %s, model: %s, sensor_id: %s, channel: %s, "
-                "temperature_c: %s, temperature_c_dec: %s, "
-                "humidity: %s, humidity_dec: %s") % (
-            self.time, self.model, self.sensor_id, self.channel,
-            self.temperature_C, self.temperature_C_dec,
-            self.humidity, self.humidity_dec)
+        return "time: %s, model: %s, sensor_id: %s, channel: %s, temperature_c: %s, temperature_c_dec: %s, humidity: %s, humidity_dec: %s" % (
+            self.time, self.model, self.sensor_id, self.channel, self.temperature_C, self.temperature_C_dec, self.humidity, self.humidity_dec)
 
     def write_sensor_data(self):
         sql = """
-        INSERT INTO sensors(
-            time, model, sensor_id, channel,
-            temperature_C, temperature_C_dec,
-            humidity, humidity_dec
-        ) VALUES (?,?,?,?,?,?,?,?)
+        INSERT INTO sensors('time','model','sensor_id','channel','temperature_C','temperature_C_dec','humidity','humidity_dec') VALUES (?,?,?,?,?,?,?,?)
         """
-        with sqlite3.connect(config.get('Database', 'path', fallback='sensors.db')) as con:
+        with sqlite3.connect(config.get('Database', 'path')) as con:
             cur = con.cursor()
             cur.execute(sql,
-                        (self.time, self.model, self.sensor_id,
-                         self.channel, self.temperature_C,
-                         self.temperature_C_dec, self.humidity,
-                         self.humidity_dec))
+                        (self.time, self.model, self.sensor_id, self.channel, self.temperature_C, self.temperature_C_dec, self.humidity, self.humidity_dec))
             con.commit()
 
 
-# MQTT Callback-Funktionen
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("Verbunden mit MQTT Broker, Code:", rc)
-    client.subscribe(MQTT_TOPIC)
-
-
-def on_message(client, userdata, msg):
-    try:
-        payload = msg.payload.decode("utf-8")
-        data = json.loads(payload)
-        sensor_data = SensorData(data)
-        print("Empfangen:", sensor_data)
-        sensor_data.write_sensor_data()
-    except Exception as e:
-        print("Fehler beim Verarbeiten der Nachricht:", e)
-
-
-# MQTT Client starten
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
-client.loop_forever()
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):
+        file = os.path.join(directory, filename)
+        lines = tuple(open(file, 'r'))
+        for line in lines:
+            data = json.loads(line)
+            sensor_data = SensorData(data)
+            sensor_data.write_sensor_data()
+        os.remove(file)
